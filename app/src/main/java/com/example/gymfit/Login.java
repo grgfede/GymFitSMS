063c8eac8c430c803ab0e485b9705de46c577f2e
Federico <federico.frari1995@gmail.com>
2020-10-07 13:12:57 +0200

    Fixed schermata login {resa responsive}

--- app/src/main/java/com/example/gymfit/Login.java
+++ app/src/main/java/com/example/gymfit/Login.java
@@ -17,6 +17,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
