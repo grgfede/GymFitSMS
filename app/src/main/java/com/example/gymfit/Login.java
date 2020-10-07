508844353286041d1224a674c93eaf09e68ae9a9
Federico <federico.frari1995@gmail.com>
2020-10-07 20:57:50 +0200

    Aggiunti SignUp2Fragment e SignUp2Fragment.java

--- app/src/main/java/com/example/gymfit/Login.java
+++ app/src/main/java/com/example/gymfit/Login.java
@@ -29,10 +29,9 @@ import com.google.firebase.auth.FirebaseUser;
 public class Login extends AppCompatActivity {
 
     EditText emailId, password;
+    TextView signUp;
     Button btnLogin;
     FirebaseAuth mFirebaseAuth;
-
-
 
 
     @Override
@@ -51,54 +50,60 @@ public class Login extends AppCompatActivity {
         //MI INSTANZIO GLI ELEMENTI
         emailId = findViewById(R.id.txtEmail);
         password = findViewById(R.id.txtPassword);
+        signUp = findViewById(R.id.textCreate);
         btnLogin = findViewById(R.id.btnLogin);
 
 
 
 
-        //CREO UN LISTENER CHE E' SEMPRE IN ASCOLTO SUL CLICK DEL BOTTONE DI LOGIN
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = emailId.getText().toString();            //MI SALVO IL VALORE DELLA TEXTBOX CHE CONTIENE LA EMAIL INSERITA DALL'UTENTE
-                String pswd = password.getText().toString();            //MI SALVO IL VALORE DELLA TEXTBOX CHE CONTIENE LA PASSWORD INSERITA DALL'UTENTE
-                if (email.isEmpty()) {
-                    emailId.setError("Attenzione! Inserisci email");
-                    emailId.requestFocus();
-                } else if (pswd.isEmpty()) {
-                    password.setError("Attenzione! Inserisci password");
-                    password.requestFocus();
-                } else if (!(email.isEmpty()) && !(pswd.isEmpty())) {
-                    //SE I CAMPI RICHIESTI SONO CORRETTAMENTE COMPILATI, VADO AD ESEGUIRE IL LOGIN
-                    mFirebaseAuth.signInWithEmailAndPassword(email, pswd).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                /*
-                                 * SE IL LOGIN VA A BUON FINE, PASSO ALL'ALTRA ACTIVITY
-                                 * E MI MEMORIZZO LE INFORMAZIONI DELL'UTENTE IN UN OGGETTO
-                                 */
-                                FirebaseUser user = mFirebaseAuth.getCurrentUser();
-                                Toast.makeText(Login.this, "Autenticazione riuscita.",
-                                        Toast.LENGTH_SHORT).show();
-                                //updateUI(user);
-                            } else {
-                                // SE IL LOGIN NON VA A BUON FINE, MOSTRO UN MESSAGGIO POPUP ALL'UTENTE PER AVVISARLO
-                                Toast.makeText(Login.this, "Autenticazione fallita.", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
+    //CREO UN LISTENER CHE E' SEMPRE IN ASCOLTO SUL CLICK DEL BOTTONE DI LOGIN
+        btnLogin.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        String email = emailId.getText().toString();            //MI SALVO IL VALORE DELLA TEXTBOX CHE CONTIENE LA EMAIL INSERITA DALL'UTENTE
+        String pswd = password.getText().toString();            //MI SALVO IL VALORE DELLA TEXTBOX CHE CONTIENE LA PASSWORD INSERITA DALL'UTENTE
+        if (email.isEmpty()) {
+            emailId.setError("Attenzione! Inserisci email");
+            emailId.requestFocus();
+        } else if (pswd.isEmpty()) {
+            password.setError("Attenzione! Inserisci password");
+            password.requestFocus();
+        } else if (!(email.isEmpty()) && !(pswd.isEmpty())) {
+            //SE I CAMPI RICHIESTI SONO CORRETTAMENTE COMPILATI, VADO AD ESEGUIRE IL LOGIN
+            mFirebaseAuth.signInWithEmailAndPassword(email, pswd).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        /*
+                         * SE IL LOGIN VA A BUON FINE, PASSO ALL'ALTRA ACTIVITY
+                         * E MI MEMORIZZO LE INFORMAZIONI DELL'UTENTE IN UN OGGETTO
+                         */
+                        FirebaseUser user = mFirebaseAuth.getCurrentUser();
+                        Toast.makeText(Login.this, "Autenticazione riuscita.",
+                                Toast.LENGTH_SHORT).show();
+                        //updateUI(user);
+                    } else {
+                        // SE IL LOGIN NON VA A BUON FINE, MOSTRO UN MESSAGGIO POPUP ALL'UTENTE PER AVVISARLO
+                        Toast.makeText(Login.this, "Autenticazione fallita.", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
-        //CODICE CHE PERMETTE DI CHIUDERE LA TASTIERA IN AUTOMATICO QUANDO SI CLICCA SULLO SCHERMO
-        findViewById(R.id.loginParentLayout).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideKeyboard(view);
-            }
-        });
+            });
+        }
     }
+    });
+
+    //CODICE CHE PERMETTE DI CHIUDERE LA TASTIERA IN AUTOMATICO QUANDO SI CLICCA SULLO SCHERMO
+    findViewById(R.id.loginParentLayout).
+
+    setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick (View view){
+            hideKeyboard(view);
+        }
+    });
+}
 
 
     public void hideKeyboard(View view) {
@@ -107,6 +112,10 @@ public class Login extends AppCompatActivity {
     }
 
 
+
+    public void signUpIntent(View v){
+        startActivity(new Intent(Login.this, SignUp.class));
+    }
 
 
 }
--- app/src/main/java/com/example/gymfit/SignUp.java
+++ app/src/main/java/com/example/gymfit/SignUp.java
@@ -1,8 +1,20 @@
 package com.example.gymfit;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class SignUp extends AppCompatActivity {
 
@@ -10,5 +22,83 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        toolbarSettings(); //MI INIZIALIZZO LE IMPOSTAZIONI DELL'ACTIONBAR
+
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragmet(new SignUpFragment());
+        pagerAdapter.addFragmet(new SignUp2Fragment());
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+
+    /*
+    *  METODO CHE MI PERMETTE DI TORNARE INDIETRO QUANDO CLICCO SULLA FRECCIA
+    */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                Intent myIntent = new Intent(SignUp.this, Login.class);
+                SignUp.this.startActivity(myIntent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+    /*
+        METODO CHE GESTISCE IL CAMBIAMENTO DEL COLORE DELLA TOOLBAR
+        In questo caso voglio che il colore della toolbar non sia
+        quello del primary color
+     */
+    private void toolbarSettings() {
+        // Definisco l'oggetto toolbar
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+
+        // Definisco l'oggetto per il colore
+        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#00000000"));
+
+        // Setto il colore della toolbar con l'oggetto colore creato prima
+        actionBar.setBackgroundDrawable(colorDrawable);
+
+        //Rimuovo il titolo dalla toolbar
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        //Setto l'icona della freccia nell'action bar
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_action_arrow_back);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+
+}
+
+
+class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    public AuthenticationPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+
+    void addFragmet(Fragment fragment) {
+        fragmentList.add(fragment);
     }
 }
