c75d1ab48fc77a2eb4a8cf3bacebfff94fec0d56
GiuseppeMondelli-Uniba <g.mondelli4@studenti.uniba.it>
2020-10-08 12:57:05 +0200

    Aggiunta e Modifica di GymFit Activity

--- app/src/main/java/com/example/gymfit/SignUp.java
+++ app/src/main/java/com/example/gymfit/SignUp.java
@@ -76,29 +76,27 @@ public class SignUp extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-
-
 }
 
 
-class AuthenticationPagerAdapter extends FragmentPagerAdapter {
-    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+    class AuthenticationPagerAdapter extends FragmentPagerAdapter {
+        private ArrayList<Fragment> fragmentList = new ArrayList<>();
 
-    public AuthenticationPagerAdapter(FragmentManager fm) {
-        super(fm);
-    }
+        public AuthenticationPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
 
-    @Override
-    public Fragment getItem(int i) {
-        return fragmentList.get(i);
-    }
+        @Override
+        public Fragment getItem(int i) {
+            return fragmentList.get(i);
+        }
 
-    @Override
-    public int getCount() {
-        return fragmentList.size();
-    }
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
 
-    void addFragmet(Fragment fragment) {
-        fragmentList.add(fragment);
+        void addFragmet(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
     }
-}
