ffa24927982ae6e2cd3b65db5f013db8e127d63a
Caterina Bianca Maggio <c.maggio8@studenti.uniba.it>
2020-10-09 12:10:43 +0200

    Profile

--- app/src/main/java/com/example/gymfit/Profile.java
+++ app/src/main/java/com/example/gymfit/Profile.java
@@ -1,4 +1,60 @@
 package com.example.gymfit;
 
-public class Profile {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.gymfit.R;
+import com.example.gymfit.user.EditProfile;
+import com.example.gymfit.user.signin.Login;
+import com.example.gymfit.user.signup.SignUp;
+import com.example.gymfit.gym.profile.GymProfile;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.widget.Button;
+
+import java.util.Objects;
+
+public class Profile extends AppCompatActivity {
+
+private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        getSupportActionBar().hide();
+
+        Button button = (Button)findViewById(R.id.Editprofile);
+
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        startActivity(new Intent(Profile.this, EditProfile.class));
+                    }
+                }
+        );
+    }
+
+    public void logoutIntent (View v) {
+        mAuth.signOut();
+        startActivity(new Intent(Profile.this, Login.class));
+    }
+
 }
+
