1737268b6677f28b3f5950b6427bff1f6c97e2c7
Caterina Bianca Maggio <c.maggio8@studenti.uniba.it>
2020-10-09 15:41:12 +0200

    Pop up immagine profilo

--- app/src/main/java/com/example/gymfit/Profile.java
+++ app/src/main/java/com/example/gymfit/Profile.java
@@ -7,10 +7,14 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -49,6 +53,23 @@ private FirebaseAuth mAuth;
                     }
                 }
         );
+
+        final ImageButton profile = (ImageButton) findViewById(R.id.profilepic);
+
+        profile.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        //startActivity(new Intent(Profile.this, EditProfile.class));
+
+                        PopupMenu popup = new PopupMenu(Profile.this, v);
+                        MenuInflater inflater = popup.getMenuInflater();
+                        inflater.inflate(R.menu.popuppic, popup.getMenu());
+                        popup.show();
+                    }
+                }
+        );
+
+
     }
 
     public void logoutIntent (View v) {
--- app/src/main/java/com/example/gymfit/user/EditProfile.java
+++ app/src/main/java/com/example/gymfit/user/EditProfile.java
@@ -2,8 +2,12 @@ package com.example.gymfit.user;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.gymfit.Profile;
 import com.example.gymfit.R;
 
 public class EditProfile extends AppCompatActivity {
@@ -14,5 +18,15 @@ public class EditProfile extends AppCompatActivity {
         setContentView(R.layout.activity_edit_profile);
 
         getSupportActionBar().hide();
+
+       /* Button button = (Button)findViewById(R.id.Backtoprofile);
+
+        button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        startActivity(new Intent(EditProfile.this, Profile.class));
+                    }
+                }
+        );*/
     }
 }
