421352cb067b9e1ba9b104c5d94860f37516f458
GiuseppeMondelli-Uniba <g.mondelli4@studenti.uniba.it>
2020-10-12 11:44:47 +0200

    Miglioramento interfaccia GymProfile

--- app/src/main/java/com/example/gymfit/gym/profile/GymProfile.java
+++ app/src/main/java/com/example/gymfit/gym/profile/GymProfile.java
@@ -1,42 +1,37 @@
 package com.example.gymfit.gym.profile;
 
+import android.app.AppComponentFactory;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gymfit.R;
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Objects;
 
-public class GymProfile extends AppCompatActivity {
+public class GymProfile extends AppCompatActivity implements OnMapReadyCallback {
     private static final String FIRE_LOG = "fire_log";
 
     private final FirebaseStorage storage = FirebaseStorage.getInstance();
@@ -52,20 +47,21 @@ public class GymProfile extends AppCompatActivity {
     private FloatingActionButton fabRound;
 
     private String userUid = null;
-    private Gym gym;
+    private Gym gym = null;
+    private GoogleMap map = null;
     private boolean clicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym_profile);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.gymMapField);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
         setUserUid();
-        setGymInterface(new GymDBCallback() {
-            @Override
-            public void onCallback(Gym gymTmp) {
-                gym = gymTmp;
-            }
-        });
+        setGymInterface(gymTmp -> gym = gymTmp);
 
         this.fab = findViewById(R.id.fab_add);
         this.fabSubscription = findViewById(R.id.fab_subscription);
@@ -76,32 +72,27 @@ public class GymProfile extends AppCompatActivity {
         this.fromButton = AnimationUtils.loadAnimation(this, R.anim.from_bottom_anim);
         this.toButton = AnimationUtils.loadAnimation(this, R.anim.to_bottom_anim);
 
-        this.fab.setOnClickListener(new View.OnClickListener() {
+        this.fab.setOnClickListener(view -> onAddButtons());
 
-            @Override
-            public void onClick(View view) {
-                onAddButtons();
-            }
+        this.fabSubscription.setOnClickListener(view -> {
+            //TODO: Open GymSubscription activity
+            Toast.makeText(GymProfile.this, "Subscription Opt", Toast.LENGTH_SHORT).show();
         });
 
-        this.fabSubscription.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                //TODO: Open GymSubscription activity
-                Toast.makeText(GymProfile.this, "Subscription Opt", Toast.LENGTH_SHORT).show();
-            }
+        this.fabRound.setOnClickListener(view -> {
+            //TODO: Open GymRound activity
+            Toast.makeText(GymProfile.this, "Round Opt", Toast.LENGTH_SHORT).show();
         });
 
-        this.fabRound.setOnClickListener(new View.OnClickListener() {
+    }
 
-            @Override
-            public void onClick(View view) {
-                //TODO: Open GymRound activity
-                Toast.makeText(GymProfile.this, "Round Opt", Toast.LENGTH_SHORT).show();
-            }
-        });
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
 
+        LatLng latLngUser = new LatLng(41.051477, 16.698150);
+        this.map.addMarker(new MarkerOptions().position(latLngUser));
+        this.map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLngUser, 15));
     }
 
     private void onAddButtons() {
@@ -149,67 +140,50 @@ public class GymProfile extends AppCompatActivity {
 
     private void setGymInterface(final GymDBCallback gymDBCallback) {
 
-        this.db.collection("gyms").document(userUid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        this.db.collection("gyms").document(userUid).get().addOnCompleteListener(task -> {
 
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+            if(task.isSuccessful()) {
+                DocumentSnapshot documentSnapshot = task.getResult();
+                assert documentSnapshot != null;
 
-                if(task.isSuccessful()) {
-                    DocumentSnapshot documentSnapshot = task.getResult();
-                    assert documentSnapshot != null;
+                TextView gymNameField = findViewById(R.id.gymNameField);
+                String name = documentSnapshot.getString("name");
+                gymNameField.setText(name);
 
-                    TextView gymNameField = findViewById(R.id.gymNameField);
-                    String name = documentSnapshot.getString("name");
-                    gymNameField.setText(name);
+                TextView gymEmailField = findViewById(R.id.gymEmailField);
+                String email = documentSnapshot.getString("email");
+                gymEmailField.setText(email);
 
-                    TextView gymEmailField = findViewById(R.id.gymEmailField);
-                    String email = documentSnapshot.getString("email");
-                    gymEmailField.setText(email);
+                TextView gymPhoneField = findViewById(R.id.gymPhoneField);
+                String phone = Objects.requireNonNull(documentSnapshot.get("phone")).toString();
+                gymPhoneField.setText(phone);
 
-                    TextView gymPhoneField = findViewById(R.id.gymPhoneField);
-                    String phone = Objects.requireNonNull(documentSnapshot.get("phone")).toString();
-                    gymPhoneField.setText(phone);
+                TextView gymAddressField = findViewById(R.id.gymAddressField);
+                HashMap<String, Object> addressFields = new HashMap<>();
+                addressFields.put("city", documentSnapshot.getString("address.city"));
+                addressFields.put("country", documentSnapshot.getString("address.country"));
+                addressFields.put("numberStreet", Objects.requireNonNull(documentSnapshot.get("address.numberStreet")).toString());
+                addressFields.put("street", documentSnapshot.getString("address.street"));
+                addressFields.put("zipCode", Objects.requireNonNull(documentSnapshot.get("address.zipCode")).toString());
+                String address = addressFields.get("street") + " " + addressFields.get("numberStreet") + ", " +
+                                addressFields.get("city");
+                gymAddressField.setText(address);
 
-                    TextView gymAddressField = findViewById(R.id.gymAddressField);
-                    HashMap<String, Object> addressFields = new HashMap<>();
-                    addressFields.put("city", documentSnapshot.getString("address.city"));
-                    addressFields.put("country", documentSnapshot.getString("address.country"));
-                    addressFields.put("numberStreet", Objects.requireNonNull(documentSnapshot.get("address.numberStreet")).toString());
-                    addressFields.put("street", documentSnapshot.getString("address.street"));
-                    addressFields.put("zipCode", Objects.requireNonNull(documentSnapshot.get("address.zipCode")).toString());
-                    String address = addressFields.get("street") + " " + addressFields.get("numberStreet") + ", " +
-                                    addressFields.get("city");
-                    gymAddressField.setText(address);
+                //TODO: get it from db
+                final ImageView gymImgField = findViewById(R.id.gymImgField);
+                StorageReference imageRef = storage.getReference().child("img/gyms/dota2.jpg");
+                long MAXBYTES = 1024 * 1024;
 
-                    final ImageView gymImgField = findViewById(R.id.gymImgField);
+                imageRef.getBytes(MAXBYTES).addOnSuccessListener(bytes -> {
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    gymImgField.setImageBitmap(bitmap);
+                }).addOnFailureListener(e -> Log.d(FIRE_LOG, "ERROR"));
 
-                    /*assert imgPath != null;
-                    Bitmap bitmap=BitmapFactory.decodeStream(getContentResolver().openInputStream(Uri.fromFile(new File(imgPath))));
-                    gymImgField.setImageBitmap(bitmap);*/
+                Gym gymTmp = new Gym(userUid, email, phone, name, addressFields);
+                gymDBCallback.onCallback(gymTmp);
 
-                    StorageReference imageRef = storage.getReference().child("img/gyms/dota2.jpg");
-                    long MAXBYTES = 1024 * 1024;
-
-                    imageRef.getBytes(MAXBYTES).addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                        @Override
-                        public void onSuccess(byte[] bytes) {
-                            Log.d(FIRE_LOG, "COMPLETE");
-                            Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                            gymImgField.setImageBitmap(bitmap);
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d(FIRE_LOG, "ERROR");
-                        }
-                    });
-
-                    Gym gymTmp = new Gym(userUid, email, phone, name, addressFields);
-                    gymDBCallback.onCallback(gymTmp);
-
-                } else {
-                    Log.d(FIRE_LOG, "ERROR: " + Objects.requireNonNull(task.getException()).getMessage());
-                }
+            } else {
+                Log.d(FIRE_LOG, "ERROR: " + Objects.requireNonNull(task.getException()).getMessage());
             }
         });
     }
